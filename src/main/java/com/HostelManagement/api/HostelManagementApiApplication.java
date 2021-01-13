02618b376294a38beaa447e34f61549c5147a4dd
jyotirmaya <jyotirmayap001@gmail.com>
2021-01-13 12:36:18 +0530

    changes in api

--- src/main/java/com/HostelManagement/api/HostelManagementApiApplication.java
+++ src/main/java/com/HostelManagement/api/HostelManagementApiApplication.java
@@ -2,12 +2,13 @@ package com.HostelManagement.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class HostelManagementApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HostelManagementApiApplication.class, args);
+		ConfigurableApplicationContext run = SpringApplication.run(HostelManagementApiApplication.class, args);
 	}
 
 }
